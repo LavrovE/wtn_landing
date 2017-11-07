35b2deadac4ff23b40990986774e75379f72e2bb
Eugene <1lavrov1@ukr.net>
2017-11-07 12:01:05 +0300

    responsive features +  titles

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 $('document').ready(function(){
 
-
-	
+$('.function__item').on('click', function(){
+	$('.function__hiddenvalue').removeClass('active');
+	$('.function__item').removeClass('active');
+	$(this).addClass('active');
+	$(this).children('.function__hiddenvalue').addClass('active');
+});
 });
